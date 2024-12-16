5f2506703749e4b2161de202cde529f25af72fa2
starkeeny <7318356+starkeeny@users.noreply.github.com>
2024-12-16 21:19:54 +0100

    add test

---
+++ source/moxi/Moxi.Tests/ProgramTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Text;
+
+namespace Moxi.Tests;
+
+[TestClass]
+public sealed class ProgramTest
+{
+    [TestMethod]
+    public void Given_Program_When_CallingMain_Then_HelloWorldPrinted()
+    {
+        // Arrange
+        var data = new StringBuilder();
+        using var textWriter = new StringWriter(data);
+
+        // Act
+        Program.MainOut(textWriter);
+
+        // Assert
+        Assert.AreEqual("Hello, World!" + Environment.NewLine, data.ToString());
+    }
+}
--- source/moxi/moxi/Program.cs
+++ source/moxi/moxi/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Moxi
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Moxi;
+
+/// <summary>
+/// Entry point class.
+/// </summary>
+public class Program
 {
-    internal class Program
+    /// <summary>
+    /// Program entry point method.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        MainOut(Console.Out);
+    }
+
+    /// <summary>
+    /// Program logic.
+    /// </summary>
+    /// <param name="consoleOut"></param>
+    public static void MainOut(TextWriter consoleOut)
+    {
+        consoleOut.WriteLine("Hello, World!");
     }
 }
